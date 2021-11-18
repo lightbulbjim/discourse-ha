d9f09b41b1350ce496d2c58426786c2e2e066703
Chris Rendle-Short <chris@killred.net>
2021-11-18 12:54:53 +1100

    Rename root module files.

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-locals {
-  region = "SGP1"
-  droplet_image = "ubuntu-20-04-x64"
-  droplet_size = "s-1vcpu-1gb"
-}
-
 resource "digitalocean_droplet" "discourse1" {
   name = "discourse1"
   region = local.region
---
+++ variables.tf
@@ -0,0 +1,5 @@
+locals {
+  region = "SGP1"
+  droplet_image = "ubuntu-20-04-x64"
+  droplet_size = "s-1vcpu-1gb"
+}
