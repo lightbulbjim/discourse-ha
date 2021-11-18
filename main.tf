1d71f7e9277d0d96995f7a6df7e6a8c32df4108d
Chris Rendle-Short <chris@killred.net>
2021-11-18 15:53:44 +1100

    Use cloud-init to provision droplet.

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "digitalocean_droplet" "discourse1" {
   name = "discourse1"
+  user_data = file("cloud-init.yaml")
   region = local.region
   image = local.droplet_image
   size = local.droplet_size
