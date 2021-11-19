dd032090556e097269665e20461e1c7b5b7e8237
Chris Rendle-Short <chris@killred.net>
2021-11-20 07:34:16 +1100

    Add load balancer.

--- main.tf
+++ main.tf
@@ -1,9 +1,34 @@
 resource "digitalocean_domain" "discourse" {
-  name = "discourse.killred.net"
+  name       = "discourse.killred.net"
+  ip_address = digitalocean_loadbalancer.public.ip
+}
+
+resource "digitalocean_certificate" "public" {
+  name    = "discourse-certificate"
+  type    = "lets_encrypt"
+  domains = ["discourse.killred.net"]
+}
+
+resource "digitalocean_loadbalancer" "public" {
+  name        = "discourse-public"
+  region      = local.region
+  droplet_tag = "discourse-app"
+
+  redirect_http_to_https = true
+  forwarding_rule {
+    entry_protocol   = "https"
+    entry_port       = 443
+    certificate_name = digitalocean_certificate.public.name
+
+    target_protocol = "http"
+    target_port     = 80
+  }
+
+  # Healthcheck once an app is running.
 }
 
 resource "digitalocean_droplet" "discourse0" {
-  name     = "discourse-primary"
+  name     = "discourse0"
   tags     = ["discourse-app"]
   region   = local.region
   image    = local.droplet_image
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "discourse0_ip" {
   value = digitalocean_droplet.discourse0.ipv4_address
 }
+
+output "loadbalancer_ip" {
+  value = digitalocean_loadbalancer.public.ip
+}
