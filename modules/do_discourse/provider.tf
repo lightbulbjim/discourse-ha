022e85f7efa702662a33d72adabebcad941449bf
Chris Rendle-Short <chris@killred.net>
2021-11-22 20:56:03 +1100

    Add all module-level provider requirements.

--- modules/do_discourse/provider.tf
+++ modules/do_discourse/provider.tf
@@ -2,7 +2,22 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
+      version = ">= 2.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.1.0"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = ">= 2.2.0"
+    }
+
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.7.2"
     }
   }
 }
